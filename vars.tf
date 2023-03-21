7aeb3fe958dec053541cfa472534e5f37b2996b9
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-03-21 22:35:58 +0530

    Update tf-module-docdb/

--- vars.tf
+++ vars.tf
@@ -4,4 +4,6 @@ variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
 variable "skip_final_snapshot" {}
 variable "engine_version" {}
+variable "subnet_ids" {}
+variable "tags" {}
 
