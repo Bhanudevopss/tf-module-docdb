0ff1755910f954924177d61aaad23f17f459e069
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-03-22 12:21:32 +0530

    Update tf-module-docdb/

--- vars.tf
+++ vars.tf
@@ -6,7 +6,10 @@ variable "skip_final_snapshot" {}
 variable "engine_version" {}
 variable "subnet_ids" {}
 variable "tags" {}
+
 variable "storage_encrypted" {
   default = true
 }
+variable "no_of_instances" {}
+variable "instance_class" {}
 
